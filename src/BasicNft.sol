0e73fb777668b7b25e5d555164f3fd90f4c65e0e
vxxxxc <veper1234@icloud.com>
2025-10-25 23:44:30 +0800

    create mapping tokenId to tokenUri, create mintNft function

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -6,17 +6,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenToUri;
 
     constructor() ERC721("BasicNft", "BNFT") {
         s_tokenCounter = 0;
     }
 
-    function mintNft(address to) external {
+    function mintNft(string memory tokenUri) external {
+        s_tokenToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter++;
-        _mint(to, s_tokenCounter);
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return "";
+        return s_tokenToUri[tokenId];
     }
 }
