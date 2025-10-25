b7f361dd10901a8b1f244b96791f3b2521566a06
vxxxxc <veper1234@icloud.com>
2025-10-25 17:36:52 +0800

    create basic erc721 contract

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,4 +2,21 @@
 
 pragma solidity ^0.8.0;
 
-contract BasicNft {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("BasicNft", "BNFT") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft(address to) external {
+        s_tokenCounter++;
+        _mint(to, s_tokenCounter);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "";
+    }
+}
